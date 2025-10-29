c293291b1e5d2befc3849b435e059ba7b296d728
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2025-10-29 19:05:03 +0530

    added python-app

---
+++ python-app/main.py
@@ -0,0 +1,9 @@
+# Simple age calculator
+
+current_year = int(input("Enter the current year: "))
+birth_year = int(input("Enter your birth year: "))
+
+age = current_year - birth_year
+
+print("You are", age, "years old.")
+
